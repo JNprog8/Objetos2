47854f95c62fd456efdf047cc6e2fe0861fb74f8
Joaco N <166165533+JNprog8@users.noreply.github.com>
2025-03-17 18:41:49 -0300

    Delete src directory

--- src/ObjetosFuertes/Main.java
+++
@@ -1,9 +0,0 @@
-package ObjetosFuertes;
-
-public class Main {
-    public static void main(String[] args) {
-        Tiempo tiempo = new Tiempo();
-        tiempo.formatoLargo();
-        tiempo.formatoCorto();
-    }
-}
